775092a291ee9cacc21dcd73eeb3ae662d234f6c
Jonathan Moallem <jdmoallem@gmail.com>
2017-06-08 10:03:18 +1000

    Initial file commit - balls exist, react to collision, space to hit ball on right vector, trying to get mouse vector to hit ball correctly

---
+++ Assets/Scripts/WhiteBallController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WhiteBallController : MonoBehaviour {
+
+    public float power;
+    private Rigidbody2D rb;
+    private Transform transform;
+
+    void Start() {
+        rb = GetComponent<Rigidbody2D>();
+        transform = GetComponent<Transform>();
+    }
+
+    void Update() {
+        if (Input.GetKeyDown("space")) {
+            Hit();
+        }
+
+        if (Input.GetMouseButtonDown(0)) {
+            var worldMousePosition = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.x, transform.position.z));
+            var direction = transform.position - worldMousePosition;
+			direction.Normalize();
+
+			rb.AddForce(direction * 1000);
+		}
+    }
+
+    void Hit() {
+    	Vector2 move = (new Vector2(100, 0)) * power;
+        rb.AddForce(move);
+    }
+}
