1eacf7333568064eb3ddfc66519376dcf8e7f209
Jonathan Moallem <jdmoallem@gmail.com>
2017-07-01 10:38:04 +1000

    Added touch and release controls to capture and apply vector to white ball

--- Assets/Scripts/WhiteBallController.cs
+++ Assets/Scripts/WhiteBallController.cs
@@ -4,31 +4,62 @@ using UnityEngine;
 
 public class WhiteBallController : MonoBehaviour {
 
-    public float power;
-    private Rigidbody2D rb;
-    private Transform transform;
+  private Rigidbody2D rb;
+  private Transform transform;
 
-    void Start() {
-        rb = GetComponent<Rigidbody2D>();
-        transform = GetComponent<Transform>();
+  public float amplification;
+
+  private float power;
+  private bool mousePressed;
+  private Vector2 mouseStartPosition;
+  private Vector2 mouseEndPosition;
+
+  private Vector2 heading;
+  private float distance;
+  private Vector2 direction;
+
+  void Start() {
+      rb = GetComponent<Rigidbody2D>();
+      transform = GetComponent<Transform>();
+  }
+
+  void Update() {
+    if (Input.GetKeyDown("space")) {
+      Hit(new Vector2(100, 0));
     }
 
-    void Update() {
-        if (Input.GetKeyDown("space")) {
-            Hit();
-        }
+    if (Input.GetMouseButtonDown(0)) {
+      mousePressed = true;
 
-        if (Input.GetMouseButtonDown(0)) {
-            var worldMousePosition = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.x, transform.position.z));
-            var direction = transform.position - worldMousePosition;
-			direction.Normalize();
+      Ray rayStart = Camera.main.ScreenPointToRay(Input.mousePosition);
 
-			rb.AddForce(direction * 1000);
-		}
+      mouseStartPosition = rayStart.origin;
     }
 
-    void Hit() {
-    	Vector2 move = (new Vector2(100, 0)) * power;
-        rb.AddForce(move);
+    if (Input.GetMouseButtonUp(0)){
+
+      if (mousePressed) {
+
+        Ray rayEnd = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        mouseEndPosition = rayEnd.origin;
+
+        heading = mouseEndPosition - mouseStartPosition;
+        distance = heading.magnitude;
+        direction = heading/distance;
+
+        Vector2 move = new Vector2(direction.x, direction.y);
+        power = move.magnitude;
+
+        Hit(move);
+
+        mousePressed = false;
+      }
+
     }
+  }
+
+  void Hit(Vector2 move) {
+    rb.AddForce(move * power * amplification);
+  }
 }
